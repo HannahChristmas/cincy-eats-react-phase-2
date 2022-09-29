3808aa540d23b48dd1eada8a7624b7292752d6f3
HannahChristmas <hc109909@gmail.com>
2022-09-29 11:54:57 -0400

    Restaurant Card Filled Out
    Fill out restaurant card with relevant details

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,7 +5,11 @@ function RestaurantCard({restaurant}){
     return(
         <div>
             <h1>{restaurant.name}</h1>
-            <h2>{restaurant.neighborhood}</h2>
+            <h3>{restaurant.neighborhood}</h3>
+            <h3>{restaurant.cuisine}</h3>
+            <ul><a href={restaurant.instagram}>Instagram</a></ul>
+            <ul><a href={restaurant.website}>Website</a></ul>
+            <ul><a href={restaurant.menu}>Menu</a></ul>
         </div>
     )
 }
