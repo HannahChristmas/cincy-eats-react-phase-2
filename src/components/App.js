c1ab19f7c9211d8d8875df7042c51c5c47ec92f3
HannahChristmas <hc109909@gmail.com>
2022-09-29 10:52:08 -0400

    Initial setup continued
    Import React & react-router-dom to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route></Route>
+      </Switch>
+      <header>
+        <h1>Welcome</h1>
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+
+
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default NavBar;
