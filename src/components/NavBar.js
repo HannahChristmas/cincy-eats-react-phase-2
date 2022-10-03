a61252d33a62f7763ad247c6bbc70e4a5ce0a82d
HannahChristmas <hc109909@gmail.com>
2022-10-03 14:24:56 -0400

    Style Navbar
    Style navbar with hover and click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,11 @@ import { NavLink } from "react-router-dom"
 
 function NavBar() {
     return (
-        <div>
-           <li>
+        <nav>
                 <NavLink to ="/" exact>Home</NavLink>
-            </li> 
-            <li>
                 <NavLink to ="/restaurants">Our Spots</NavLink>
-            </li> 
-            <li>
                 <NavLink to ="/add">Add A Spot</NavLink>
-            </li>
-        </div>
+        </nav>
     )
 }
 
