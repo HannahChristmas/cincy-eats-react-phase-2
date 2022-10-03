97b331b4c9af0893ae1a312e89ee456c5d618dcc
HannahChristmas <hc109909@gmail.com>
2022-10-03 15:36:11 -0400

    Import Home Page Images
    Import images for about/home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import red from '../red.jpeg'
+import pastry from "../pastry.jpeg"
+import egg from "../egg.jpeg"
+import taco from "../taco.jpeg"
+
 
 function Home() {
 
     return<div className="home-div">
         <h1>About Us</h1>
         <p>Love Cincinnati? So do we. Use this app as a food-passport and keep track of your favorite spots. One of your favorites isn't on the list? Go ahead and add it!</p>
-        <img src="https://img.theculturetrip.com/768x/smart/wp-content/uploads/2018/03/2007325.jpg"/>
+        {/* <img src="https://img.theculturetrip.com/768x/smart/wp-content/uploads/2018/03/2007325.jpg"/> */}
+        <img class="homeImages"src={red}/>
+        <img class="homeImages"src={pastry}/>
+        <img class="homeImages"src={egg}/>
+        <img class="homeImages"src={taco}/>
     </div>
 }
 
